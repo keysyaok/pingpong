0edb1a07d2d128df3dfe1198bdff960be28bb31a
keysyaok <keysyaalexandra1@gmail.com>
2025-09-04 20:34:50 +0700

    Add files via upload

--- ping/ok.py
+++ ping/ok.py
@@ -45,6 +45,10 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < height - 80:
             self.rect.y += self.speed
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1: LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2: LOSE!', True, (180, 0, 0))
 
 p1 = Player('itu.png',30, 200, 30, 100, 5)
 p2 = Player('itu.png',520, 200, 30, 100, 5)
@@ -56,26 +60,40 @@ speed_x = 3
 speed_y = 3
 
 game = True 
+finish = False
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False 
     
-    window.fill(background_color)
+    if finish != True: 
+        window.fill(background_color)
 
-    p1.reset()
-    p2.reset()
-    ball.reset()
+        p1.reset()
+        p2.reset()
+        ball.reset()
 
-    p1.move_p1()
-    p2.move_p2()
+        p1.move_p1()
+        p2.move_p2()
 
-    ball.rect.x += speed_x
-    ball.rect.y += speed_y
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-    if ball.rect.y > height - 50:
-        speed_y *= -1
+        if ball.rect.y > height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(p1, ball) or sprite.collide_rect(p2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > width:
+            finish = True
+            window.blit(lose2, (200, 200))
 
     display.update()
     clock.tick(60)
